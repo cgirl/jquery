6a30eee3f4560d52d191ee389c58596a36cd4aa8
cgirl <1579090779@qq.com>
2016-01-31 12:41:57 +0800

    jquery中ajax新闻实例与数组

---
+++ 124/01-test.php
@@ -0,0 +1,9 @@
+<?php
+header("Content-type:text/html;charset=utf-8");
+
+echo '[
+		{"title":"11111111", "pubTime":"2016-1-31"},
+		{"title":"22222222", "pubTime":"2016-1-30"},
+		{"title":"33333333", "pubTime":"2016-1-29"}
+	]';
+?>

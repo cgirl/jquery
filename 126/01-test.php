e23864fafb77b8920563a935e707df899edd6840
cgirl <1579090779@qq.com>
2016-01-31 14:18:06 +0800

    jquery中ajax与XML数据处理

---
+++ 126/01-test.php
@@ -0,0 +1,13 @@
+<?php
+header("Content-type:text/xml;charset=utf-8");
+
+$str = '
+<root>
+	<title>111111111111111</title>
+	<title>222222222222222</title>
+	<title>333333333333333</title>
+</root>
+';
+
+echo $str;
+?>
